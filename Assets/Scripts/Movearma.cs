18827d4f097a5053d7fc3017c32d03204cfa7427
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-02-28 19:38:43 -0300

    Movimenta Arma Atualizado

--- Assets/Scripts/Movearma.cs
+++ Assets/Scripts/Movearma.cs
@@ -9,7 +9,7 @@ public class Movearma : MonoBehaviour
  
     void Update()
     {
-        rotacaoVertical = Input.GetAxis("Mouse Y") * velocidadeRotacao;
+        rotacaoVertical += Input.GetAxis("Mouse Y") * velocidadeRotacao;
         float rotacaoX = Mathf.Clamp(-rotacaoVertical, -45, 45);
         transform.localEulerAngles = new Vector3(rotacaoX, transform.localEulerAngles.y, transform.localEulerAngles.z);
         

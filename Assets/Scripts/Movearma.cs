f422cc9bcaf0acd2fed6dd0c644f18cb3888601a
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-02-28 19:21:01 -0300

    Arma
    Arma Rotação

---
+++ Assets/Scripts/Movearma.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movearma : MonoBehaviour
+{
+    public float velocidadeRotacao;
+    private float rotacaoVertical;
+ 
+    void Update()
+    {
+        rotacaoVertical = Input.GetAxis("Mouse Y") * velocidadeRotacao;
+        float rotacaoX = Mathf.Clamp(-rotacaoVertical, -45, 45);
+        transform.localEulerAngles = new Vector3(rotacaoX, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        
+    }
+}
